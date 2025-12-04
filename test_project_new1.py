1afe34263ee4e75e9b8488c64c70dcded9b7f164
rishithaagan070890 <rishithaagan070890@gmail.com>
2025-12-04 16:58:31 +0530

    modified compose file to support chrome browser

--- test_project_new1.py
+++ test_project_new1.py
@@ -3,9 +3,9 @@ import time
 from select import select
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.options import Options
+from selenium.webdriver.chrome.options import Options
 
-from selenium.webdriver.support.ui import Select
+
 
 
 
@@ -14,11 +14,14 @@ def test_validlogin():
     options = Options()
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.set_capability("browserName", "chrome")
 
     driver = webdriver.Remote(
-        command_executor="http://selenium-grid:4444/wd/hub",
-        options=options
+         command_executor="http://localhost:4444/wd/hub",
+         options=options,
+
     )
+
 
     driver.get("https://practicetestautomation.com/practice-test-login/")
     driver.find_element(By.XPATH,'//input[@name="username"]').send_keys("student")
